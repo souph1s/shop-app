3f6a27f2511e3d79ff77114045ec540f38771305
souph1s <sophiamperez@hotmail.com>
2024-07-24 20:18:36 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,31 @@
+import Image from "next/image";
 import { styled } from "../styles";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const Button = styled("button", {
-  backgroundColor: "$green300",
-  borderRadius: 4,
-  border: 0,
-  padding: "4px 8px",
-
-  span: {
-    fontWeight: "bold",
-  },
-
-  "&:hover": {
-    filter: "brightness(0.8)",
-  },
-});
+import tshirt1 from "../assets/tshirts/1.png";
+import tshirt2 from "../assets/tshirts/2.png";
+import tshirt3 from "../assets/tshirts/3.png";
 
 export default function Home() {
   return (
-    <Button>
-      <span>Test</span>
-      Submit
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={tshirt1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>T-Shirt x</strong>
+          <span>$79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={tshirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>T-Shirt x</strong>
+          <span>$79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
