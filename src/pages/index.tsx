930dcc8669d21b77763a2e32244c04ddf1fab0fc
souph1s <sophiamperez@hotmail.com>
2024-07-25 22:13:22 -0300

    feat: format price to dollar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,9 @@ import { useKeenSlider } from "keen-slider/react";
 
 import { HomeContainer, Product } from "../styles/pages/home";
 
-import tshirt1 from "../assets/tshirts/1.png";
-import tshirt2 from "../assets/tshirts/2.png";
-import tshirt3 from "../assets/tshirts/3.png";
-
 import "keen-slider/keen-slider.min.css";
 import { stripe } from "../lib/stripe";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Stripe from "stripe";
 
 interface HomeProps {
@@ -49,7 +45,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -62,13 +58,17 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount! / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: "currency",
+        currency: 'USD',
+      }).format(price.unit_amount! / 100),
     }
   })
 
   return {
     props: {
       products,
-    }
+    },
+    revalidate: 60 * 60 * 2 // 2 hours
   };
 };
