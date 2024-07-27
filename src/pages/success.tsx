e1d00727c40917479386331c6cfc7dfd8232c366
souph1s <sophiamperez@hotmail.com>
2024-07-27 00:41:48 -0300

    feat: success page structure

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
-  return <div>Success</div>;
+  return (
+    <SuccessContainer>
+      <h1>Thank you for your order!</h1>
+
+      <ImageContainer>
+
+
+      </ImageContainer>
+
+      <p>Yay! <strong>Diego Fernandes</strong>, your brand new <strong>T-Shirt</strong> is on your way!</p>
+
+      <Link href='/'>
+        Back to shop
+      </Link>
+
+    </SuccessContainer>
+  );
 }
